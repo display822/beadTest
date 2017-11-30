8e1ccea0f8aca3a8673582c187a2f8eec9866105
display <13720335094@163.com>
2017-11-30 16:11:56 +0800

    签名文件

--- app/src/main/java/com/dreamwallet/Application.java
+++ app/src/main/java/com/dreamwallet/Application.java
@@ -17,6 +17,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import cn.jpush.android.api.JPushInterface;
 import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 /**
@@ -27,6 +28,8 @@ public class Application extends BaseApplication {
     public void onCreate() {
         super.onCreate();
         ViewTarget.setTagId(com.dreamwallet.R.id.tag_glide);
+        JPushInterface.setDebugMode(false);
+        JPushInterface.init(this);
         setBaseUrl();
         initShare();
         initUmengStatistics();
